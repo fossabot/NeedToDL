518449ce3c41337d32c1ea5c34820f0aa8ac9b8c
Hernik_plays <hernik@hernikplays.cz>
2019-08-27 22:00:12 +0200

    App can now download YouTube videos in MP4 or webm(audio) (because i cant convert it in code)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,59 @@
 var fs = require('fs');
 var ytdl = require('ytdl-core');
 var mkdirp = require('mkdirp');
-//var testurl = "http://www.youtube.com/watch?v=A02s8omM_hI";
+const homedir = require('os').homedir().replace(/\\/g, "/");
 
 
 function submit() {
     var link = document.getElementById('ytlink').value
     var log = document.getElementById('log');
-    if (!fs.existsSync("./videos")) {
-        mkdirp('./videos', function(err) { if (err != null) return alert(err) });
+    var format = document.getElementById('mpsel').selectedIndex
+    var dl = document.getElementById('dlsel').selectedIndex
+    if (!fs.existsSync("./dl")) {
+        mkdirp('./dl', function(err) { if (err != null) return alert(err) });
     }
-    ytinfo(link);
-
+    if (dl == "0") {
+        ytinfo(link, format);
+    } else if (dl == "1") {
+        dlsptf(link)
+    }
 
 }
 
-function ytinfo(url) {
+function ytinfo(url, format) {
     var validate = ytdl.validateURL(url)
     if (validate != true) return alert("Not valid URL")
     ytdl.getInfo(url, (err, info) => {
         if (err) return alert(err);
-        console.log(info.author.name + info.title)
-        download(info, url)
+        console.log(info)
+        downloadYT(info, url, format)
     })
 
 }
 
-function download(info, url) {
-    var author = info.author.name.split(' ').join('_')
-    var title = info.title.split(' ').join('_')
-    ytdl(url, { filter: (format) => format.container === 'mp4' })
-        .pipe(fs.createWriteStream(`./videos/${author}-${title}.mp4`));
+function dlsptf(link) {
+    return ("not working yet")
 }
 
+function downloadYT(info, url, format) {
+    var author = info.author.name.split(' ').join('_')
+    var title = info.title.split(' ').join('_')
+    if (format == "0") {
+
+        ytdl(url, { filter: (format) => format.container === "mp4" })
+            .pipe(fs.createWriteStream(`./dl/${author}-${title}.mp4`));
+    } else if (format == "1") {
+        //a
+        ytdl(url, { filter: "audioonly" })
+            .pipe(fs.createWriteStream(`./dl/${author}-${title}.webm`));
+
+    }
+}
+
+
+
 function printit(s) {
+    var log = document.getElementById('log');
+
     log.innerHTML += s + '<br>';
 }
